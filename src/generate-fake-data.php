bcf1929696fb075b4c8fc0a0deb4d98d130460a1
Facundo J Gonzalez <gonzalezfj@unsj-cui.edu.ar>
2018-01-07 12:39:07 -0300

    prueba

--- src/generate-fake-data.php
+++ src/generate-fake-data.php
@@ -24,14 +24,5 @@ class Colectivo extends \Illuminate\Database\Eloquent\Model {
   protected $table = 'colectivo';
 }
 
-echo Colectivo::All()->toJson();
 
 $faker = Faker\Factory::create();
-
-// generate data by accessing properties
-echo $faker->name;
-  // 'Lucy Cechtelar';
-echo $faker->address;
-  // "426 Jordy Lodge
-  // Cartwrightshire, SC 88120-6700"
-echo $faker->text;
